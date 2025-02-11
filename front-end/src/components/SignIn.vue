27a1fbd9f07ca895e67333b3c928e2f6de6c6b6d
Adi-K527 <adikandakurtikar2@gmail.com>
2025-02-10 20:16:29 -0600

    frontend-deployment

--- front-end/src/components/SignIn.vue
+++ front-end/src/components/SignIn.vue
@@ -26,7 +26,7 @@
       <i class="fab fa-facebook"></i>
     </div>
     <div class="links">
-      <p>Don't have an account yet? skjdfskdjfhksdjfhksjdf</p>
+      <p>Don't have an account yet?</p>
       <button @click="$emit('changeView', 'SignUp')">Sign Up</button>
     </div>
   </div>
