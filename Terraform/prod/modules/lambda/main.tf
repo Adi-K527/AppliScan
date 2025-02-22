d16d82d9576ad38f3685e378b8928a230ff20b7f
Adi-K527 <adikandakurtikar2@gmail.com>
2025-02-22 16:28:10 -0600

    updated tf files

--- Terraform/prod/modules/lambda/main.tf
+++ Terraform/prod/modules/lambda/main.tf
@@ -63,4 +63,5 @@ resource "aws_lambda_function" "lambda_standard" {
   role             = aws_iam_role.iam_for_lambda.arn
   filename         = data.archive_file.lambda_zip[0].output_path
   source_code_hash = data.archive_file.lambda_zip[0].output_base64sha256
+  timeout          = "180"
 }
