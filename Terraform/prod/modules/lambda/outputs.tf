f6c06628f0c3ff7a9bd402456e1a6ab93b86fc0a
Adi-K527 <adikandakurtikar2@gmail.com>
2025-02-22 14:25:22 -0600

    kinesis tf

--- Terraform/prod/modules/lambda/outputs.tf
+++ Terraform/prod/modules/lambda/outputs.tf
@@ -1,3 +1,3 @@
 output "lambda_arn" {
-    value = var.container_based ? aws_lambda_function.lambda_container_based[0].lambda_invoke_arn : aws_lambda_function.lambda_standard[0].lambda_invoke_arn
+    value = var.container_based ? aws_lambda_function.lambda_container_based[0].invoke_arn : aws_lambda_function.lambda_standard[0].invoke_arn
 }
