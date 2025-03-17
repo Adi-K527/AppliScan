b556e5e4df2e9e232d4e3c1607ea27e616a36c5f
Adi-K527 <adikandakurtikar2@gmail.com>
2025-03-17 13:35:42 -0500

    cognito

--- Terraform/prod/modules/cognito/main.tf
+++ Terraform/prod/modules/cognito/main.tf
@@ -23,9 +23,3 @@ resource "aws_cognito_user_pool_domain" "appliscan_domain" {
   domain       = "appliscan-${random_id.suffix.hex}" 
   user_pool_id = aws_cognito_user_pool.appliscan_user_pool.id
 }
-
-resource "aws_cognito_user_pool_ui_customization" "cognito_ui" {
-  user_pool_id = aws_cognito_user_pool.appliscan_user_pool.id
-  client_id    = aws_cognito_user_pool_client.appliscan_app_client.id
-  css          = file("${var.css_file_path}")
-}
