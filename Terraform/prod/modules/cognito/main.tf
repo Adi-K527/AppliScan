bebcd2e6f4c7faadf742a44bfa93b2a199011271
Adi-K527 <adikandakurtikar2@gmail.com>
2025-03-17 13:09:36 -0500

    cognito

--- Terraform/prod/modules/cognito/main.tf
+++ Terraform/prod/modules/cognito/main.tf
@@ -31,3 +31,14 @@ resource "aws_cognito_user_pool_ui_customization" "cognito_ui" {
   client_id    = aws_cognito_user_pool_client.appliscan_app_client.id
   css          = ".label-customizable {font-weight: 400;}"
 }
+
+resource "aws_cognito_identity_provider" "cognito_identity_provider" {
+  user_pool_id      = aws_cognito_user_pool.appliscan_user_pool.id
+  provider_name     = "COGNITO"
+  provider_type     = "COGNITO"
+  provider_details = {
+    authorize_scopes = "email"
+    client_id        = aws_cognito_user_pool_client.appliscan_app_client.id
+    client_secret    = aws_cognito_user_pool_client.appliscan_app_client.client_secret
+  }
+}
