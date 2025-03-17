e31339eb5893b90d195d242dd80c93b9c8e05d97
Adi-K527 <adikandakurtikar2@gmail.com>
2025-03-17 12:51:38 -0500

    cognito

--- Terraform/prod/modules/cognito/main.tf
+++ Terraform/prod/modules/cognito/main.tf
@@ -8,3 +8,8 @@ resource "aws_cognito_user_pool_client" "appliscan_app_client" {
   callback_urls = [ var.cognito_redirect_url ]
   depends_on    = [ aws_cognito_user_pool.appliscan_user_pool ]
 }
+
+resource "aws_cognito_user_pool_ui_customization" "cognito_ui" {
+  user_pool_id = aws_cognito_user_pool.appliscan_user_pool.id
+  client_id    = aws_cognito_user_pool_client.appliscan_app_client.id
+}
