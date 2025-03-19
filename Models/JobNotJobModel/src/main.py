8546ba36f1ebf98b1da902ff869018b590f1ae19
Adi-K527 <adikandakurtikar2@gmail.com>
2025-03-19 18:36:06 -0500

    updated firehose function

--- Models/JobNotJobModel/src/main.py
+++ Models/JobNotJobModel/src/main.py
@@ -22,5 +22,5 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps({"prediction": prediction[0]}, default=np_encoder)
+        'body': json.dumps({"prediction": prediction}, default=np_encoder)
     }
--- Terraform/prod/code-files/firehose/lambda_function.py
+++ Terraform/prod/code-files/firehose/lambda_function.py
@@ -5,21 +5,43 @@ import base64
 def lambda_handler(event, context):
     record_id = event['records'][0]['recordId']
 
+    print(event['records'])
+
+    compiled_records = []
+
     emails = []
+    ids    = [] 
     uuids  = []
 
     for record in event['records']:
-       uuids.append(str(uuid.uuid4()))
-       emails.append(record['data'])
+       record_body = json.loads(base64.b64decode(record['data']).decode("utf-8"))
+
+       cur_id = record_body[0]['id']
+       cur_emails = [i['body'] for i in record_body]
+
+       compiled_records.append({
+           'id':       cur_id,
+           'emails':   cur_emails
+       })
+
+    for record in compiled_records:
+        emails.extend(record['emails'])
+        ids.extend([record['id'] for i in record['emails']])
+        uuids.extend([str(uuid.uuid4()) for i in record['emails']])
 
     transformed_data = base64.b64encode(json.dumps({
         'emails': emails,
+        'ids':    ids,
         'uuids':  uuids
     }).encode("utf-8")).decode("utf-8")
     
     print(transformed_data)
     return {
-        'recordId': record_id,
-        'result':   'Ok',
-        'records':  transformed_data
+        "records": [
+            {
+                'recordId': record_id,
+                'result':   'Ok',
+                'data':  transformed_data
+            }
+        ]
     }
