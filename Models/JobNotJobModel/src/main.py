ecf380c2c7dab8cbba602a7e067570ee159b8c1f
Adi-K527 <adikandakurtikar2@gmail.com>
2025-03-19 19:12:39 -0500

    smaller docker container

--- Models/JobNotJobModel/src/main.py
+++ Models/JobNotJobModel/src/main.py
@@ -11,14 +11,24 @@ def np_encoder(object):
 
 
 def lambda_handler(event, context):
+
+    print("-------------------------------------    LOG 1   -------------------------------------")
+    print(event["body"])
     
     bucket = boto3.resource('s3', 
                             aws_access_key_id=os.getenv('MY_AWS_ACCESS_THING'), 
                             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS')).Bucket(os.getenv('AWS_BUCKET'))
     
     bucket.download_file('Job_related_Model.joblib', '/tmp/model.joblib')
-    model = joblib.load('/tmp/model.joblib')        
+    model = joblib.load('/tmp/model.joblib')      
+
+    print("-------------------------------------    LOG 2   -------------------------------------")
+    print(model)
+
     prediction = model.predict([event["body"]])
+
+    print("-------------------------------------    LOG 3   -------------------------------------")
+    print(prediction)
     
     return {
         'statusCode': 200,
