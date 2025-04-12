f7bd8b37ccfa752752045b0eed3c82575fedb51c
Adi-K527 <adikandakurtikar2@gmail.com>
2025-04-12 13:32:05 -0500

    model code changes

--- Models/JobStatusModel/src/main.py
+++ Models/JobStatusModel/src/main.py
@@ -3,11 +3,6 @@ import boto3
 import joblib
 import numpy as np
 
-
-def np_encoder(object):
-    if isinstance(object, np.generic):
-        return object.item()
-
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
@@ -15,21 +10,13 @@ def lambda_handler(event, context):
     print("-------------------------------------    LOG 1   -------------------------------------")
     event = json.loads(event['Records'][0]['body'])
     event = json.loads(event['Message'])
-    print("------ Message -----")
-    print(event)
-
-    print("------ Message, responsePayload -----")
-    print(event['responsePayload'])
-
-    print("------ Message, responsePayload, body -----")
-    print(event['responsePayload']['body'])
     event = json.loads(event['responsePayload']['body'])
 
-    # print("------ Message, responsePayload, body full event -----")
-    print("by gods grace")
     print(event['data'])
 
     emails = [i[0] for i in event['data']]
+    ids    = [i[1] for i in event['data']]
+    uuids  = [i[2] for i in event['data']]
 
     s3_client.download_file(Bucket   = "appliscan-bucket-325", 
                             Key      = "Job_Status_Preprocessing_Pipeline.joblib", 
@@ -50,8 +37,20 @@ def lambda_handler(event, context):
 
     print("-------------------------------------    LOG 3   -------------------------------------")
     print(prediction)
+    prediction = [i.item() for i in prediction]
+
+    result = []
+    for i in range(len(prediction)):
+        result.append([prediction[i], ids[i], uuids[i], emails[i]])
+
+    with open('output.json', 'w') as f:
+        json.dump(result, f, indent=2)
+
+    s3_client.upload_file(Filename = 'output.json',
+                          Bucket   = "appliscan-model-output-bucket", 
+                          Key      = "Job_Status/prediction.json")
     
     return {
         'statusCode': 200,
-        'body': json.dumps({"prediction": prediction[0]}, default=np_encoder)
+        'body': json.dumps({"Result": result})
     }
