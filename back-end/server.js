50ceff0040f9d8b6f60fc2adb1d00d3cfc83f07b
Adi-K527 <adikandakurtikar2@gmail.com>
2025-04-12 17:51:22 -0500

    glue and backend changes

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pg = require('pg');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 
